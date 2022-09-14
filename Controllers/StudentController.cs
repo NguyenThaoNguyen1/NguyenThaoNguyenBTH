94f1db23b8ee1dba7fb9974250c7f64e6467a82c
NguyenThaoNguyen1 <nguyenthaonguyen20102001@gmail.com>
2022-09-14 21:28:39 +0700

    14/09

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -15,16 +15,23 @@ public class StudentController : Controller
             {
                 new Student{ StudentID=1, StudentName="Nguyễn Văn A", Age=14},
                 new Student{ StudentID=2, StudentName="Nguyễn Văn B", Age=19},
-                new Student{ StudentID=3, StudentName="Nguyễn Văn H", Age=25},
-                new Student{ StudentID=4, StudentName="Nguyễn Văn K", Age=17},
+                new Student{ StudentID=3, StudentName="Nguyễn Văn C", Age=25},
+                new Student{ StudentID=4, StudentName="Nguyễn Văn D", Age=17},
                 new Student{ StudentID=5, StudentName="Nguyễn Văn E", Age=10},
-                new Student{ StudentID=5, StudentName="Nguyễn Văn E", Age=15},
+                new Student{ StudentID=6, StudentName="Nguyễn Văn E", Age=15},
+                 new Student{ StudentID=7, StudentName="Nguyễn Văn H", Age=14},
+                new Student{ StudentID=8, StudentName="Nguyễn Văn M", Age=19},
+                new Student{ StudentID=9, StudentName="Nguyễn Văn P", Age=25},
+                new Student{ StudentID=10, StudentName="Nguyễn Văn Q", Age=17},
+                new Student{ StudentID=11, StudentName="Nguyễn Văn T", Age=10},
+                new Student{ StudentID=15, StudentName="Nguyễn Văn E", Age=15},
 
 
 
             };
-            ViewBag.DanhsachSV = StdList.Count();
-            return View();
+            // ViewBag.DanhsachSV = StdList.Count();
+            ViewData["Student"] = StdList;
+            return View(StdList);
         }
 
         [HttpGet]
